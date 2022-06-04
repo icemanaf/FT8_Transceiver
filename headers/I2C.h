dad6f200a69c0662ec6df1d01ebca4dc93bbbe49
icemanaf <dilantha.ariyapala@googlemail.com>
2022-06-04 16:10:11 +0100

    initial commit

---
+++ headers/I2C.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   I2C.h
+ * Author: Ashith
+ *
+ * Created on 04 June 2022, 16:06
+ */
+
+#ifndef I2C_H
+#define	I2C_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* I2C_H */
+
---
+++ source/I2C.c
@@ -0,0 +1,1 @@
+#include "../headers/I2C.h"
